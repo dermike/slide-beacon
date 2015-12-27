69da9af5388857d305f52d6015b1d2f6fc826da8
Mikael Jergefelt <mikael.jergefelt@ki.se>
2015-12-27 12:36:44 +0100

    lint

--- revealjs-plugin/slide-beacon.js
+++ revealjs-plugin/slide-beacon.js
@@ -1,38 +1,36 @@
-(function() {
-  
-  if ((typeof(WebSocket) == 'undefined') && (typeof(MozWebSocket) != 'undefined')) {
+(function slidebeacon() {
+  if (typeof WebSocket === 'undefined' && typeof MozWebSocket !== 'undefined') {
     WebSocket = MozWebSocket;
   }
-  
-  function getUrl(slide) {
-    var slideElement = Reveal.getCurrentSlide();
-    var url = slideElement.getAttribute('data-url');
+
+  function getUrl() {
+    var slideElement = Reveal.getCurrentSlide(),
+      url = slideElement.getAttribute('data-url');
     if (url && ws) {
       ws.send(url);
     }
   }
-  
-  Reveal.addEventListener('ready', function() {
+
+  Reveal.addEventListener('ready', function ready() {
     var server = prompt('Enter slide beacon host/ip', 'localhost');
     server = server || 'localhost';
-    
+
     ws = new WebSocket('ws://' + server + ':1234/');
-    ws.onopen = function(event) {
+    ws.onopen = function onopen() {
       console.log('Connected to beacon-server');
       getUrl();
     };
-    ws.onmessage = function(event) {
+    ws.onmessage = function onmessage(event) {
       console.log(event.data);
-    }
-    ws.onclose = function(event) {
+    };
+    ws.onclose = function onclose() {
       ws = null;
       console.log('Connection closed');
     };
-    ws.onerror = function(event) {
+    ws.onerror = function onerror() {
       console.log('Received error');
     };
   });
-  
+
   Reveal.addEventListener('slidechanged', getUrl);
-  
 })();
